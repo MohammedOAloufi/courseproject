e25080fff196e52385c6c066d5b32b6670a7b21d
Mohammed Aloufi <Mohammed-Aloufi1422@hotmail.com>
2026-02-08 01:52:57 +0300

    add logout desing the page of login and sign in

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from . import views
+from django.contrib.auth.views import LogoutView
+
 
 urlpatterns = [
     path("register/", views.register_view, name="register"),
     path("login/", views.login_view, name="login"),
+    path("logout/", LogoutView.as_view(next_page="home"), name="logout"),
+
 ]
